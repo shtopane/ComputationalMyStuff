4a96f8e9d89b79f069ae0a5e9d23422f7c628399
krasi <vechesumstudent@gmail.com>
2022-05-02 16:33:01 +0200

    plotted likelihood and log-likelihood for all parameters

--- classification_problem_set.R
+++ classification_problem_set.R
@@ -17,9 +17,10 @@ mylikelihood <- function(beta) {
   
   linear_model <- beta[1] + beta[2] * x + beta[3] * x2
   # prod((exp(linear_model) / (1 + exp(linear_model)))^y * (1 - (exp(linear_model)/(1 - exp(linear_model))))^(1 - y))
-  likelihood <- prod((exp(linear_model) / (1 + exp(linear_model))) ^ y * (1 - (exp(linear_model) / (
-    1 + exp(linear_model)
-  ))) ^ (1 - y))
+  likelihood <-
+    prod((exp(linear_model) / (1 + exp(linear_model))) ^ y * (1 - (exp(linear_model) / (
+      1 + exp(linear_model)
+    ))) ^ (1 - y))
   # likelihood <- prod((prob_i_x^y) * ((1 - prob_i_x)^(1 - y))) # this behaves the same as (2)
   return(likelihood)
 }
@@ -76,20 +77,123 @@ prob_i_x <- exp(linear_model) / (1 + exp(linear_model))
 y <- rbinom(n = length(x), size = 1, prob = prob_i_x)
 
 # c) Plot the likelihood function and the log-likelihood function for a range of values for the two parameters
-# separately and show that they are maximized at the same value.
-plotlike <- function(x){
-  betas <- c(beta0, beta1, x)
-  result <- mylikelihood(betas)
+# separately and show that they are maximized at the same value. ----
+
+# c) 1. Likelihood
+# evaluate_for = "likelihood / loglikelihood"
+plotlike <- function(evaluate_for, beta0, beta1, beta2) {
+  #betas <- c(beta0, beta1, x)
+  betas <- c(beta0, beta1, beta2)
+  result <- NULL
+  
+  if (evaluate_for == "likelihood") {
+    result <- mylikelihood(betas)
+  } else if (evaluate_for == "loglikelihood") {
+    result <- myloglikelihood(betas)
+  }
+  
   return(result)
 }
 
+# General variables for plotting
 expr_plotlike <- Vectorize(plotlike)
-result <- curve(expr = expr_plotlike, from= 0, to=2.00)
-plot(result)
-lines(result, col="red")
-max_of_result <- result$x[which(result$y==(max(result$y,na.rm = TRUE)))]
-abline(v = max_of_result)
-# Testing functions from https://www.r-bloggers.com/2019/08/maximum-likelihood-estimation-from-scratch/
+evaluated_function_name <- "likelihood"
+beta_0_to_from <- list(from = -3.00,
+                       to = 0)
+beta_1_to_from <- list(from = 0,
+                       to = 0.1897)
+beta_2_to_from <- list(from = 0,
+                       to = 2.00)
+
+# For beta0
+
+result_beta0 <- curve(
+  expr = expr_plotlike(evaluated_function_name, x, beta1, beta2),
+  from = beta_0_to_from$from,
+  to = beta_0_to_from$to,
+  xlab = "Range of beta 0",
+  ylab = "mylikelihood(beta0)",
+  main = "Evaluating Likelihood function for beta0"
+)
+max_of_result_beta0 <-
+  result_beta0$x[which(result_beta0$y == (max(result_beta0$y, na.rm = TRUE)))]
+max_of_result_beta0
+abline(v = max_of_result_beta0)
+
+# For beta 1
+result_beta1 <- curve(
+  expr = expr_plotlike(evaluated_function_name, beta0, x, beta2),
+  from = beta_1_to_from$from,
+  to = beta_1_to_from$to,
+  xlab = "Range of beta 1",
+  ylab = "mylikelihood(beta1)",
+  main = "Evaluating Likelihood function for beta1"
+)
+max_of_result_beta1 <-
+  result_beta1$x[which(result_beta1$y == (max(result_beta1$y, na.rm = TRUE)))]
+max_of_result_beta1
+abline(v = max_of_result_beta1)
+
+# For beta 2
+result_beta2 <-
+  curve(
+    expr = expr_plotlike(evaluated_function_name, beta0, beta1, x),
+    from = beta_2_to_from$from,
+    to = beta_2_to_from$to,
+    xlab = "Range of beta 2",
+    ylab = "mylikelihood(beta2)",
+    main = "Evaluating Likelihood function for beta2"
+  )
+max_of_result_beta2 <-
+  result_beta2$x[which(result_beta2$y == (max(result_beta2$y, na.rm = TRUE)))]
+max_of_result_beta2
+abline(v = max_of_result_beta2)
+
+# c) 2. Log-likelihood
+# For beta0
+evaluated_function_name <- "loglikelihood"
+result_log_beta_0 <-
+  curve(
+    expr = expr_plotlike(evaluated_function_name, x, beta1, beta2),
+    from = beta_0_to_from$from,
+    to = beta_0_to_from$to,
+    xlab = "Range of beta 0",
+    ylab = "myloglikelihood(beta0)",
+    main = "Evaluating Log-Likelihood function for beta0"
+  )
+max_of_result_log_beta_0 <-
+  result_log_beta_0$x[which(result_log_beta_0$y == (max(result_log_beta_0$y, na.rm = TRUE)))]
+max_of_result_log_beta_0
+abline(v = max_of_result_log_beta_0)
+# For beta1
+result_log_beta_1 <-
+  curve(
+    expr = expr_plotlike(evaluated_function_name, beta0, x, beta2),
+    from = beta_1_to_from$from,
+    to = beta_1_to_from$to,
+    xlab = "Range of beta 1",
+    ylab = "myloglikelihood(beta1)",
+    main = "Evaluating Log-Likelihood function for beta1"
+  )
+max_of_result_log_beta_1 <-
+  result_log_beta_1$x[which(result_log_beta_1$y == (max(result_log_beta_1$y, na.rm = TRUE)))]
+max_of_result_log_beta_1
+abline(v = max_of_result_log_beta_1)
+# For beta2
+result_log_beta_2 <-
+  curve(
+    expr = expr_plotlike(evaluated_function_name, beta0, beta1, x),
+    from = beta_2_to_from$from,
+    to = beta_2_to_from$to,
+    xlab = "Range of beta 2",
+    ylab = "myloglikelihood(beta2)",
+    main = "Evaluating Log-Likelihood function for beta2"
+  )
+max_of_result_log_beta_2 <-
+  result_log_beta_2$x[which(result_log_beta_2$y == (max(result_log_beta_2$y, na.rm = TRUE)))]
+max_of_result_log_beta_2
+abline(v = max_of_result_log_beta_2)
+# End for c) -----
 
 library(maxLik)
 # Getting an estimate for both functions
@@ -97,12 +201,13 @@ start_param <- c(0, 1, 1)
 loglikelihood.estimate <- maxBFGS(myloglikelihood,
                                   finalHessian = TRUE,
                                   start = start_param)
+loglikelihood.estimate$estimate
 ### TODO: Pass previous estimation as this function's start parameter? This is cheating maybe
 likelihood.estimate <- maxBFGS(mylikelihood,
                                finalHessian = TRUE,
                                start = loglikelihood.estimate$estimate)
 
-
+likelihood.estimate$estimate
 
 likelihood.covariance_matrix <-
   -(solve(likelihood.estimate$hessian))
@@ -164,11 +269,11 @@ loglikelihood.result1 <- getMarginalEffectBeta1(
 lines(x, loglikelihood.result1$dx_container, col = "red")
 
 # Plot probabilities
-plot(x, loglikelihood.result$probability_container, main = "Estimated Probabilities")
-lines(x, loglikelihood.result1$probability_container, col = "red")
-
-plot(x, loglikelihood.result$probability_container, ylim = c(0, 1))
-lines(x, loglikelihood.result1$probability_container, col = "red")
+# plot(x, loglikelihood.result$probability_container, main = "Estimated Probabilities")
+# lines(x, loglikelihood.result1$probability_container, col = "red")
+# 
+# plot(x, loglikelihood.result$probability_container, ylim = c(0, 1))
+# lines(x, loglikelihood.result1$probability_container, col = "red")
 
 # TEST f)?
 logit <- function(x) {
