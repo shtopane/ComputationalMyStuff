b64a567ea5825957e299d7bf3e05fe8691b63bb5
krasi <vechesumstudent@gmail.com>
2022-05-22 12:35:46 +0200

    Done 2b by adding adjustment to sigma for class 2(don't see any real difference though..)

--- Problem Set 3/linear_discriminant_analysis.R
+++ Problem Set 3/linear_discriminant_analysis.R
@@ -4,6 +4,8 @@ library("mvtnorm")
 library("MASS")
 library("ggplot2")
 
+set.seed(555)
+
 # CONSTANTS
 class1_name <- "Class 1"
 class2_name <- "Class 2"
@@ -13,11 +15,11 @@ class1_n <- 300
 class2_n <- 500
 sigma_cap <- rbind(c(16, -2), c(-2, 9))
 
-generate_data_lda <- function(class1_n, class2_n, class1_mu, class2_mu, sigma_cap) {
+generate_data_lda <- function(class1_n, class2_n, class1_mu, class2_mu, class1_sigma_cap = sigma_cap, class2_sigma_cap = sigma_cap) {
     class_type <- factor(c(rep(class1_name, class1_n), rep(class2_name, class2_n)))
 
-    class1_x <- mvrnorm(class1_n, class1_mu, sigma_cap) # nolint
-    class2_x <- mvrnorm(class2_n, class2_mu, sigma_cap) # nolint
+    class1_x <- mvrnorm(class1_n, class1_mu, class1_sigma_cap) # nolint
+    class2_x <- mvrnorm(class2_n, class2_mu, class2_sigma_cap) # nolint
     together <- rbind(class1_x, class2_x)
 
     data <- data.frame(class_type, "C1" = together[, 1], "C2" = together[, 2])
@@ -25,7 +27,7 @@ generate_data_lda <- function(class1_n, class2_n, class1_mu, class2_mu, sigma_ca
     return(data)
 }
 
-train_data <- generate_data_lda(class1_n, class2_n, class1_mu, class2_mu, sigma_cap)
+train_data <- generate_data_lda(class1_n, class2_n, class1_mu, class2_mu)
 
 plot1 <- ggplot(train_data, aes(C1, C2, color = class_type)) +
     geom_point()
@@ -69,7 +71,7 @@ train_lda_error <- get_average_prediction_error(train_data$class_type, train_lda
 train_logit_error <- get_average_prediction_error(train_data$class, train_logit_class_type)
 
 # Generate test data, predict and calculate error
-test_data <- generate_data_lda(class1_n, class2_n, class1_mu, class2_mu, sigma_cap)
+test_data <- generate_data_lda(class1_n, class2_n, class1_mu, class2_mu)
 
 test_lda_predict <- predict(train_lda, newdata = test_data)
 test_logit_predict <- predict(train_logit, newdata = test_data, type = "response")
@@ -87,10 +89,10 @@ train_logit_error_container <- rep(NA, simulation_runs)
 test_lda_error_container <- rep(NA, simulation_runs)
 test_logit_error_container <- rep(NA, simulation_runs)
 
-for(i in 1:simulation_runs){
-    train_data <- generate_data_lda(class1_n, class2_n, class1_mu, class2_mu, sigma_cap)
-    test_data <- generate_data_lda(class1_n, class2_n, class1_mu, class2_mu, sigma_cap)
-    
+for (i in 1:simulation_runs) {
+    train_data <- generate_data_lda(class1_n, class2_n, class1_mu, class2_mu)
+    test_data <- generate_data_lda(class1_n, class2_n, class1_mu, class2_mu)
+
     # Estimate lda + logit for train data
     train_lda <- lda(class_type ~ C1 + C2, data = train_data)
     train_lda_predict <- predict(train_lda, newdata = train_data)
@@ -98,11 +100,11 @@ for(i in 1:simulation_runs){
     train_logit <- glm(class_type ~ C1 + C2, data = train_data, family = "binomial")
     train_logit_predict <- predict(train_logit, newdata = train_data, type = "response")
     train_logit_predict_class_type <- get_class_logit(train_logit_predict, class1_name, class2_name)
-    print(length(train_data))
+
     # Calculate errors
     train_lda_error_container[i] <- get_average_prediction_error(train_data$class_type, train_lda_predict$class)
     train_logit_error_container[i] <- get_average_prediction_error(train_data$class_type, train_logit_predict_class_type)
-    print(length(train_lda_error_container))
+
     # Estimate lda + logit for test data
     test_lda_predict <- predict(train_lda, newdata = test_data)
     test_logit_predict <- predict(train_logit, newdata = test_data, type = "response")
@@ -122,14 +124,15 @@ plot(
     main = "LDA Error rate for Train and Test data",
     col = "blue",
     pch = 21
-   )
+)
 points(test_lda_error_container, col = "red")
 abline(h = mean(train_lda_error_container))
 abline(h = mean(test_lda_error_container))
 legend(
-    x = "topright",
+    x = "topleft",
     bty = "n",
-    legend = c("Train", "Test"), lty = c(1, 1), col = c("blue", "red"))
+    legend = c("Train", "Test"), lty = c(1, 1), col = c("blue", "red")
+)
 
 
 # Plot Logit errors
@@ -141,13 +144,63 @@ plot(
     main = "Logit Error rate for Train and Test data",
     col = "blue",
     pch = 21
-   )
+)
 points(test_logit_error_container, col = "red")
 abline(h = mean(train_logit_error_container))
 abline(h = mean(test_logit_error_container))
 legend(
-    x = "topright",
+    x = "topleft",
     bty = "n",
-    legend = c("Train", "Test"), lty = c(1, 1), col = c("blue", "red"))
+    legend = c("Train", "Test"), lty = c(1, 1), col = c("blue", "red")
+)
 
 # 2.b) Attempt to worsen the relative performance of LDA by allowing the covariance matrices to differ
+sigma_adjustment_factor <- seq(1, 10, by = 0.2)
+sigma_adjustment_factor_length <- length(sigma_adjustment_factor)
+# error_length <- sigma_adjustment_factor_length * simulation_runs
+
+test_lda_new_error_container <- rep(NA, simulation_runs)
+test_logit_new_error_container <- rep(NA, simulation_runs)
+test_lda_new_error_mean_container <- rep(NA, sigma_adjustment_factor_length)
+test_logit_new_error_mean_container <- rep(NA, sigma_adjustment_factor_length)
+
+for (i in 1:sigma_adjustment_factor_length) {
+    # Compute new sigma matrix, adjusted by the current adjustment factor
+    sigma_adjusted_class_2 <- sigma_cap * sigma_adjustment_factor[i]
+
+    for (j in 1:simulation_runs) {
+        train_data <- generate_data_lda(
+            class1_n,
+            class2_n,
+            class1_mu,
+            class2_mu,
+            class1_sigma_cap = sigma_cap,
+            class2_sigma_cap = sigma_adjusted_class_2
+        )
+
+        test_data <- generate_data_lda(
+            class1_n,
+            class2_n,
+            class1_mu,
+            class2_mu,
+            class1_sigma_cap = sigma_cap,
+            class2_sigma_cap = sigma_adjusted_class_2
+        )
+
+        train_lda <- lda(class_type ~ C1 + C2, data = train_data)
+        train_logit <- glm(class_type ~ C1 + C2, data = train_data, family = "binomial")
+
+        test_lda_predict <- predict(train_lda, newdata = test_data)
+        test_logit_predict <- predict(train_logit, newdata = test_data, type = "response")
+        test_logit_predict_class_type <- get_class_logit(test_logit_predict, class1_name, class2_name)
+
+        test_lda_new_error_container[j] <- get_average_prediction_error(train_data$class_type, test_lda_predict$class)
+        test_logit_new_error_container[j] <- get_average_prediction_error(train_data$class_type, test_logit_predict_class_type)
+    }
+
+    test_lda_new_error_mean_container[i] <- mean(test_lda_new_error_container)
+    test_logit_new_error_mean_container[i] <- mean(test_logit_new_error_container)
+}
+
+plot(1:sigma_adjustment_factor_length, test_lda_new_error_mean_container, type = "l")
+lines(test_logit_new_error_mean_container, col = "red")
